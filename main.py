4e8f72aa2dd619e718bbd3f66d0562dc12035f42
l-natali <lukienko.natasha@gmail.com>
2022-07-24 23:24:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+import telebot
+import os
+
+
+app = Flask(__name__)
+TOKEN = os.environ.get('TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handlers(commands=['start'])
+def message_start(message):
+    bot.send_message(message.chat.id, 'Hello user')
+
+
+@bot.message_handlers(commands=['links'])
+def message_links(message):
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+
+    with open('python_links.txt') as file:
+        links = [item.split(',') for item in file]
+
+        for title, link in links:
+            url_button = telebot.types.InlineKeyboardButton(text=title.strip(), url=link.strip())
+            keyboard.add(url_button)
+
+        bot.send_message(message.chat.id, 'List of links', reply_markup=keyboard)
+
+
+@bot.message_handlers(func=lambda x: x.text.lower().startwith('python'))
+def message_text(message):
+    bot.send_message(message.chat.id, 'Python')
+
+
+@app.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "Python Telegram Bot", 200
+
+
+@app.route('/')
+def main():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://test-bot-python-jule.herokuapp.com/' + TOKEN)
+    return 'Python Telegram Bot', 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
